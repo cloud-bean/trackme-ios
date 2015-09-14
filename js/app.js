1539857afd0c1dbf2f6bfc713f5de9582b152b19
hygkui <fevernova.life@gmail.com>
2015-09-14 20:25:19 +0800

    去掉自定义的directive，直接写

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.filter', 'starter.directives'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.filter', 'starter.common'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -33,48 +33,48 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','s
 
   // setup an abstract state for the tabs directive
     .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
 
-  // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+    .state('tab.dash', {
+      url: '/dash',
+      views: {
+        'tab-dash': {
+          templateUrl: 'templates/tab-dash.html',
+          controller: 'DashCtrl'
+        }
       }
-    }
-  })
-  
-  .state('tab.groups', {
-    url: '/groups',
-    views: {
-      'tab-groups': {
-        templateUrl: 'templates/tab-groups.html',
-        controller: 'CarGroupCtrl'
+    })
+    
+    .state('tab.groups', {
+      url: '/groups',
+      views: {
+        'tab-groups': {
+          templateUrl: 'templates/tab-groups.html',
+          controller: 'CarGroupCtrl'
+        }
       }
-    }
-  })
-   
-  .state('cars',{
-    url: '/cars/:id',
-    templateUrl: 'templates/car.html',
-    controller:'carCtrl'
-  })
+    })
+     
+    .state('cars',{
+      url: '/cars/:id',
+      templateUrl: 'templates/car.html',
+      controller:'carCtrl'
+    })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+    .state('tab.setting', {
+      url: '/setting',
+      views: {
+        'tab-setting': {
+          templateUrl: 'templates/tab-setting.html',
+          controller: 'SettingCtrl'
+        }
       }
-    }
-  });
+    });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
